5e6ce95d7bf6fc620819919e4457fda34c5fe283
Hyeontaek Lim <hl@cs.cmu.edu>
2017-09-30 20:34:58 -0400

    Raise notebook logging level

--- scripts/run_notebook.sh
+++ scripts/run_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jupyter notebook --ip=0.0.0.0 --port=48888 --no-browser --notebook-dir=$(dirname "$0")/../exptools/sample
+jupyter notebook --log-level=ERROR --ip=0.0.0.0 --port=48888 --no-browser --notebook-dir=$(dirname "$0")/../exptools/sample
