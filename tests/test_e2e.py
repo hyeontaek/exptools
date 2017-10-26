dafbfbe113219f4e7f6c7a619b0bf7b64616a2f9
Hyeontaek Lim <hl@cs.cmu.edu>
2017-10-26 18:09:53 -0400

    Use full path for server fixture

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -20,33 +20,39 @@ async def loop(event_loop):
 async def server(unused_tcp_port, loop):
   '''Run a exptools server.'''
   port = unused_tcp_port
-  argv = [f'--port={port}']
   ready_event = asyncio.Event()
 
-  cwd = os.getcwd()
+  with tempfile.TemporaryDirectory() as tmp_dir:
+    server_args = [
+        f'--host=127.0.0.1',
+        f'--port={port}',
+        f'--secret-file={tmp_dir}/secret.json',
+        f'--scheduler-file={tmp_dir}/sched_conf.json',
+        f'--history-file={tmp_dir}/history.json',
+        f'--queue-file={tmp_dir}/queue.json',
+        f'--output-dir={tmp_dir}/output',
+        ]
 
-  try:
-    with tempfile.TemporaryDirectory() as tmp_dir:
-      print(f'Using temporary directory: {tmp_dir}')
-      os.chdir(tmp_dir)
+    task = asyncio.ensure_future(run_server(server_args, ready_event=ready_event, loop=loop), loop=loop)
+    await ready_event.wait()
 
-      task = asyncio.ensure_future(run_server(argv, ready_event=ready_event, loop=loop), loop=loop)
-      await ready_event.wait()
+    yield {
+        'client_args': [
+          f'--host=127.0.0.1',
+          f'--port={port}',
+          f'--secret-file={tmp_dir}/secret.json',
+          ]
+        }
 
-      yield {'port_arg': f'--port={port}'}
-
-      task.cancel()
-      try:
-        await task
-      except concurrent.futures.CancelledError:
-        pass
-
-  finally:
-    os.chdir(cwd)
+    task.cancel()
+    try:
+      await task
+    except concurrent.futures.CancelledError:
+      pass
 
 async def run(server, args, stdin=None, loop=None):
   '''Run a exptools client'''
-  args = [server['port_arg']] + list(args)
+  args = server['client_args'] + list(args)
   return await run_client(args, loop=loop)
 
 @pytest.mark.asyncio
