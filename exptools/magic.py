031a0a9bac73debc31c11ffb67569c926471c674
Hyeontaek Lim <hl@cs.cmu.edu>
2017-11-05 19:02:54 -0500

    Add gc command

--- exptools/magic.py
+++ exptools/magic.py
@@ -3,6 +3,7 @@
 __all__ = ['Magic']
 
 import asyncio
+import gc
 import logging
 import os
 import sys
@@ -27,7 +28,7 @@ class Magic:
 
     while True:
       if not os.path.exists(self.path):
-        await asyncio.sleep(60, loop=self.loop)
+        await asyncio.sleep(10, loop=self.loop)
         continue
 
       try:
@@ -35,7 +36,11 @@ class Magic:
           cmd = await file.read()
           cmd = cmd.strip()
 
-          if cmd == 'stack':
+          if cmd == 'gc':
+            print('garbage collected', file=sys.stderr)
+            gc.collect()
+
+          elif cmd == 'stack':
             tasks = asyncio.Task.all_tasks(loop=self.loop)
             print('=' * 20, file=sys.stderr)
             print('', file=sys.stderr)
