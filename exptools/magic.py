eb927a8f9ee3a26c6df6c6c0431874be1ebe0b26
Hyeontaek Lim <hl@cs.cmu.edu>
2017-11-05 19:27:45 -0500

    Monitor magic file; Add task filtering

--- exptools/magic.py
+++ exptools/magic.py
@@ -3,12 +3,14 @@
 __all__ = ['Magic']
 
 import asyncio
+import concurrent
 import gc
 import logging
 import os
 import sys
 
 import aiofiles
+import aionotify
 
 
 class Magic:
@@ -23,37 +25,51 @@ class Magic:
 
   async def run_forever(self):
     '''Process magic commands.'''
-    if os.path.exists(self.path):
-      os.unlink(self.path)
+    open(self.path, 'w').close()
 
-    while True:
-      if not os.path.exists(self.path):
-        await asyncio.sleep(10, loop=self.loop)
-        continue
-
-      try:
-        async with aiofiles.open(self.path) as file:
-          cmd = await file.read()
-          cmd = cmd.strip()
-
-          if cmd == 'gc':
-            print('garbage collected', file=sys.stderr)
-            gc.collect()
-
-          elif cmd == 'stack':
-            tasks = asyncio.Task.all_tasks(loop=self.loop)
-            print('=' * 20, file=sys.stderr)
-            print('', file=sys.stderr)
-            for task in tasks:
-              task.print_stack(file=sys.stderr)
-              print('', file=sys.stderr)
-            print('=' * 20, file=sys.stderr)
-
-          else:
-            self.logger.error(f'Unrecognized: {cmd}', file=sys.stderr)
-
-      finally:
+    watcher = aionotify.Watcher()
+    watcher.watch(self.path, aionotify.Flags.CLOSE_WRITE)
+    await watcher.setup(self.loop)
+    try:
+      while True:
         try:
-          os.unlink(self.path)
-        except OSError:
-          self.logger.exception('Cannot unlink magic file')
+          await watcher.get_event()
+          await self._handle()
+          self.logger.debug(f'Detected status change for magic file')
+        except concurrent.futures.CancelledError:
+          # Break loop (likely normal exit through task cancellation)
+          break
+        except Exception:  # pylint: disable=broad-except
+          self.logger.exception(f'Exception while watching magic file')
+    finally:
+      watcher.unwatch(self.path)
+      watcher.close()
+
+  async def _handle(self):
+    async with aiofiles.open(self.path) as file:
+      cmd = await file.read()
+      cmd = cmd.strip().split(' ')
+
+      if cmd[0] == 'gc':
+        print('garbage collected', file=sys.stderr)
+        gc.collect()
+
+      elif cmd[0] == 'stack':
+        tasks = asyncio.Task.all_tasks(loop=self.loop)
+        if cmd[1:]:
+          if cmd[1] == 'running':
+            tasks = [task for task in tasks if not task.done()]
+          elif cmd[1] == 'done':
+            tasks = [task for task in tasks if task.done()]
+          elif cmd[1] == 'cancelled':
+            tasks = [task for task in tasks if task.cancelled()]
+
+        print('=' * 20, file=sys.stderr)
+        print('', file=sys.stderr)
+        for task in tasks:
+          task.print_stack(file=sys.stderr)
+          print('', file=sys.stderr)
+        print('=' * 20, file=sys.stderr)
+
+      else:
+        print(f'Unrecognized: {cmd}', file=sys.stderr)
