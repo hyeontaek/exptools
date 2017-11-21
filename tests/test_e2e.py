afd726cd2d5b5126e975bf21408fa32debbbb3d8
Hyeontaek Lim <hl@cs.cmu.edu>
2017-11-21 05:32:47 -0500

    Fix server registry file path for tests; Avoid closing event_loop prematurely; Add test_add

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,5 +1,7 @@
 import asyncio
 import concurrent
+import json
+import os
 import re
 import tempfile
 
@@ -10,10 +12,10 @@ from exptools.client_main import client_main
 
 
 @pytest.fixture
-async def loop(event_loop):
+def loop(event_loop):
   """Provide an alias for event_loop fixture."""
   yield event_loop
-  event_loop.stop()
+  # Do not close event_loop; it will be done by the event_loop fixture
 
 
 @pytest.fixture
@@ -28,42 +30,85 @@ async def server(unused_tcp_port, loop):
       f'--port={port}',
       f'--secret-file={tmp_dir}/secret.json',
       f'--scheduler-file={tmp_dir}/sched_conf.json',
+      f'--registry-file={tmp_dir}/registry.json',
       f'--history-file={tmp_dir}/history.json',
       f'--queue-file={tmp_dir}/queue.json',
       f'--output-dir={tmp_dir}/output',
+      # f'--verbose',
       ]
 
     task = asyncio.ensure_future(
       server_main(server_args, ready_event=ready_event, loop=loop), loop=loop)
-    await ready_event.wait()
 
-    yield {
-      'client_args': [
-        f'--host=127.0.0.1',
-        f'--port={port}',
-        f'--secret-file={tmp_dir}/secret.json',
-        ]
-      }
-
-    task.cancel()
     try:
-      await task
-    except concurrent.futures.CancelledError:
-      pass
+      await ready_event.wait()
+
+      yield {
+        'server_task': task,
+        'client_args': [
+          f'--host=127.0.0.1',
+          f'--port={port}',
+          f'--secret-file={tmp_dir}/secret.json',
+          # f'--verbose',
+          ],
+        'tmp_dir': tmp_dir,
+        }
+    finally:
+      task.cancel()
+      try:
+        await task
+      except concurrent.futures.CancelledError:
+        pass
 
 
-async def run(server, args, loop=None):
+async def run(server, args, loop):
   """Run a exptools client"""
   args = server['client_args'] + list(args)
   return await client_main(args, loop=loop)
 
 
+def create_params_file(server, params):
+  """Create a params file."""
+  params_json_path = os.path.join(server['tmp_dir'], 'params.json')
+
+  with open(params_json_path, 'w') as file:
+    json.dump(params, file)
+
+  return params_json_path
+
+
+async def add_params(server, paramset, params, loop):
+  """Add params to a new paramset."""
+  params_json_path = create_params_file(server, params)
+  await run(server, ['add', '-c', '-f', params_json_path, paramset], loop=loop)
+
+
 @pytest.mark.asyncio
-async def test_s(capsys, loop, server):
+async def test_s(capsys, server, loop):
   await run(server, ['--color=no', 's'], loop=loop)
   stdout, stderr = capsys.readouterr()
   # pytest -s
   # print(stdout)
+  # print(stderr)
   assert re.search(
     r'^S:0 F:0 A:0 Q:0  Remaining 0s  Finish by .*  Concurrency 1\.0$',
-    stdout) is not None
+    stdout, re.MULTILINE) is not None
+
+
+@pytest.mark.asyncio
+async def test_add(capsys, server, loop):
+  paramset = 'params'
+  params = [
+    {
+      'command': 'ls',
+    },
+  ]
+  await add_params(server, paramset, params, loop=loop)
+  stdout, stderr = capsys.readouterr()
+  # pytest -s
+  # print(stdout)
+  # print(stderr)
+  assert re.search(r'^Added: %s$' % paramset, stdout, re.MULTILINE) is not None
+  assert re.search(
+    r'^Added: %d parameters to %s$' % (len(params), paramset),
+    stdout, re.MULTILINE) is not None
